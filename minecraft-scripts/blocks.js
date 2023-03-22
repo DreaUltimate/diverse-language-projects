f8b4bb481e5954dae99bb10c011007acba463b07
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-03-22 14:12:03 +0800

    Improve block handling and registry
    This commit improves the block handling and registry by creating a Block class with private properties and getter methods, adding error handling to functions that receive a block as input, and organizing all blocks in a block registry. Additionally, the code now follows better naming conventions and is more modular, making it easier to maintain and expand in the future. These changes improve the quality of the code and make it more resilient to potential errors.

--- minecraft-scripts/blocks.js
+++ minecraft-scripts/blocks.js
@@ -1,61 +1,86 @@
 // Define a new class for custom blocks
 class Block {
-  constructor(name, texture, hardness, durability) {
-    this.name = name;
-    this.texture = texture;
-    this.hardness = hardness;
-    this.durability = durability;
-  }
+constructor(name, texture, hardness, durability) {
+this._name = name;
+this._texture = texture;
+this._hardness = hardness;
+this._durability = durability;
+}
+
+get name() {
+return this._name;
+}
+
+get texture() {
+return this._texture;
+}
+
+get hardness() {
+return this._hardness;
+}
+
+get durability() {
+return this._durability;
+}
 }
 
 // Create some new custom blocks
 const stoneBricks = new Block("Stone Bricks", "textures/block/stone_bricks.png", 1.5, 30);
 const glass = new Block("Glass", "textures/block/glass.png", 0.3, 3);
 
-// Add the new blocks to the game's block registry
+// Create a block registry to store all blocks in the game
 const blockRegistry = {
-  // Existing vanilla blocks
-  dirt: {name: "Dirt", texture: "textures/block/dirt.png", hardness: 0.5, durability: 15},
-  grass: {name: "Grass", texture: "textures/block/grass.png", hardness: 0.6, durability: 15},
-  stone: {name: "Stone", texture: "textures/block/stone.png", hardness: 1.5, durability: 30},
-  // New custom blocks
-  stone_bricks: stoneBricks,
-  glass: glass
+dirt: new Block("Dirt", "textures/block/dirt.png", 0.5, 15),
+grass: new Block("Grass", "textures/block/grass.png", 0.6, 15),
+stone: new Block("Stone", "textures/block/stone.png", 1.5, 30),
+stone_bricks: stoneBricks,
+glass: glass
 };
 
 // Update the game's block rendering function to handle the new blocks
-function renderBlock(blockType) {
-  const texture = blockType.texture;
-  // ...
+function renderBlock(block) {
+if (!block instanceof Block) {
+throw new Error("Invalid block type");
+}
+const texture = block.texture;
+// ...
 }
 
 // Update the game's block breaking function to handle the new blocks
-function breakBlock(blockType) {
-  const hardness = blockType.hardness;
-  const durability = blockType.durability;
-  // ...
+function breakBlock(block) {
+if (!block instanceof Block) {
+throw new Error("Invalid block type");
+}
+const hardness = block.hardness;
+const durability = block.durability;
+// ...
 }
 
 // Allow players to craft the new blocks
-function craftBlock(blockType) {
-  // ...
+function craftBlock(block) {
+if (!block instanceof Block) {
+throw new Error("Invalid block type");
+}
+// ...
 }
 
 // Update the game's world generation code to include the new blocks
 function generateWorld() {
-  // ...
-  if (someCondition) {
-    setBlock(x, y, z, blockRegistry.stone_bricks);
-  }
-  // ...
+// ...
+if (someCondition) {
+setBlock(x, y, z, blockRegistry.stone_bricks);
+}
+// ...
 }
 
 // Add the new blocks to the game's user interface
 function updateUI() {
-  // ...
-  for (const blockType in blockRegistry) {
-    const name = blockRegistry[blockType].name;
-    const texture = blockRegistry[blockType].texture;
-    // ...
-  }
+// ...
+for (const blockType in blockRegistry) {
+if (blockRegistry.hasOwnProperty(blockType)) {
+const name = blockRegistry[blockType].name;
+const texture = blockRegistry[blockType].texture;
+// ...
+}
+}
 }
