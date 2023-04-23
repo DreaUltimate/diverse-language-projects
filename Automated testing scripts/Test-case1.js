6280b73ed7d5030bbf87469c3670eaad3ba94b7d
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-04-23 17:50:12 +0800

    Update Test-case1.js

--- Automated testing scripts/Test-case1.js
+++ Automated testing scripts/Test-case1.js
@@ -1,27 +1,27 @@
 const addNumbers = (a, b) => a + b;
 
 describe('addNumbers', () => {
-  it('should add two positive numbers correctly', () => {
+  test('should add two positive numbers correctly', () => {
     const result = addNumbers(2, 3);
-    expect(result).toBe(5);
+    expect(result).toEqual(5);
   });
 
-  it('should add a positive and negative number correctly', () => {
+  test('should add a positive and negative number correctly', () => {
     const result = addNumbers(5, -3);
-    expect(result).toBe(2);
+    expect(result).toEqual(2);
   });
 
-  it('should add two negative numbers correctly', () => {
+  test('should add two negative numbers correctly', () => {
     const result = addNumbers(-2, -3);
-    expect(result).toBe(-5);
+    expect(result).toEqual(-5);
   });
 
-  it('should return NaN if one of the inputs is not a number', () => {
+  test('should return NaN if one of the inputs is not a number', () => {
     const result = addNumbers('hello', 2);
     expect(result).toBeNaN();
   });
 
-  it('should throw an error if no arguments are passed', () => {
+  test('should throw an error if no arguments are passed', () => {
     expect(() => addNumbers()).toThrow();
   });
 });
