269e1e2b45b963639926edcf5a14643daa073da0
Dius Montenegro <114764373+DreaUltimate@users.noreply.github.com>
2023-04-23 17:47:06 +0800

    Update Test-case1.js

--- Automated testing scripts/Test-case1.js
+++ Automated testing scripts/Test-case1.js
@@ -3,19 +3,31 @@ function addNumbers(a, b) {
 }
 
 describe('addNumbers', () => {
-  it('adds two positive numbers correctly', () => {
-    expect(addNumbers(2, 3)).toBe(5);
+  it('should add two positive numbers correctly', () => {
+    const result = addNumbers(2, 3);
+    expect(result).toBe(5);
   });
 
-  it('adds a positive and negative number correctly', () => {
-    expect(addNumbers(5, -3)).toBe(2);
+  it('should add a positive and negative number correctly', () => {
+    const result = addNumbers(5, -3);
+    expect(result).toBe(2);
   });
 
-  it('adds two negative numbers correctly', () => {
-    expect(addNumbers(-2, -3)).toBe(-5);
+  it('should add two negative numbers correctly', () => {
+    const result = addNumbers(-2, -3);
+    expect(result).toBe(-5);
   });
 
-  it('returns NaN if one of the inputs is not a number', () => {
-    expect(addNumbers('hello', 2)).toBe(NaN);
+  it('should return NaN if one of the inputs is not a number', () => {
+    const result = addNumbers('hello', 2);
+    expect(result).toBeNaN();
+  });
+
+  it('should throw an error if no arguments are passed', () => {
+    expect(() => addNumbers()).toThrow();
   });
 });
+In this improved version, the it block descriptions use the word "should" to indicate the expected behavior of the function. Additionally, each test case assigns the result of calling the addNumbers function to a variable
+
+
+
